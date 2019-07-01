852ac93d2534c0cc4153f2d3cc6b26f66fefee0f
Bobby <bobbyhplau@gmail.com>
2019-07-01 21:23:35 +0000

    Finished Exercise

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+    // Sum all the numbers in the array
+
+    if (array.length === 0) {
+        return 0;
+    }
+
+    let popped = array.pop();
+
+    if (Array.isArray(popped)) {
+        return sumItems(popped) + sumItems(array);
+    } else {
+        return popped + sumItems(array);
+    }
 }
 
 module.exports = sumItems;
